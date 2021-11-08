39bace01f7922e436d8e6903a7e22d63b61558c2
Martin <github@mx.schlatter.eu>
2021-11-08 10:37:15 +0100

    Update parser.py
    sometimes I got an exception because "features" was not in data.

--- src/rki_covid_parser/parser.py
+++ src/rki_covid_parser/parser.py
@@ -37,7 +37,8 @@ def generator_attributes_from_features(data):
     _features = "features"
     _attributes = "attributes"
 
-    for feature in data[_features]:
+    if _features in data:
+      for feature in data[_features]:
         assert _attributes in feature
         yield feature[_attributes]
 
